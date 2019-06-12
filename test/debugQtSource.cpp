8bcb8136f836c9c174bdb5a67cf7baaef0c0a65c
onlyet <1369212327@qq.com>
2019-06-12 17:34:41 +0800

    '1'

--- test/debugQtSource.cpp
+++ test/debugQtSource.cpp
@@ -1,8 +1,14 @@
 #include "common.h"
 #include <QPushButton>
+#include <QLabel>
 
 void debugQtSource()
 {
-    QPushButton* btn = new QPushButton;
-    btn->show();
+    //QPushButton* btn = new QPushButton;
+    //btn->show();
+    //QLabel* l = new QLabel;
+    //QWidget* w = new QWidget;
+    //w->show();
+    QObject* obj = new QObject;
+    
 }
